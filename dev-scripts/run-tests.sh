9e76aaca8f72c6955dc0632953e66f30979aa199
brainfoolong <brainfoolong@users.noreply.github.com>
2023-04-01 22:00:21 +0200

    upload all userdata artifacts instead of a few

--- dev-scripts/run-tests.sh
+++ dev-scripts/run-tests.sh
@@ -63,7 +63,7 @@ if [ $TESTTYPE == "phpunit" ]; then
   INI_PATH=/etc/php/8.2/cli/conf.d/99-phpunit.ini
   cecho b "# Php Unit Tests"
   docker $DOCKER_EXECPARAMS "printf 'zend_extension=xdebug.so\nxdebug.mode=coverage\nmemory_limit=-1' > $INI_PATH"
-  docker $DOCKER_EXECPARAMS "cd /framelix/appdata && composer update && framelix_php vendor/bin/phpunit --coverage-clover /framelix/userdata/tmp/clover.xml --bootstrap modules/FramelixTests/tests/_bootstrap.php --configuration  modules/FramelixTests/tests/_phpunit.xml -d zend_extension=xdebug.so -d xdebug.mode=coverage -d memory_limit=-1 && framelix_php hooks/after-phpunit.php && rm -f /etc/php/*/*/conf.d/*-phpunit.ini"
+  docker $DOCKER_EXECPARAMS "cd /framelix/appdata && composer update && framelix_php vendor/bin/phpunit --coverage-clover /framelix/userdata/tmp/clover.xml --bootstrap modules/FramelixTests/tests/_bootstrap.php --configuration  modules/FramelixTests/tests/_phpunit.xml -d zend_extension=xdebug.so -d xdebug.mode=coverage -d memory_limit=-1 && framelix_php hooks/after-phpunit.php"
   docker $DOCKER_EXECPARAMS "rm -f $INI_PATH"
   exit $?
 fi
