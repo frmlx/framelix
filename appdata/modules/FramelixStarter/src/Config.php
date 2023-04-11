ad0c07d0e627c065f3ad3947758a9f4c85e2f4b3
brainfoolong <brainfoolong@users.noreply.github.com>
2023-04-11 14:57:26 +0200

    moved default db filepath to dbdata folder

--- appdata/modules/FramelixStarter/src/Config.php
+++ appdata/modules/FramelixStarter/src/Config.php
@@ -2,15 +2,13 @@
 
 namespace Framelix\FramelixStarter;
 
-use Framelix\Framelix\Utils\FileUtils;
-
 class Config
 {
     public static function onRegister(): void
     {
         \Framelix\Framelix\Config::addSqliteConnection(
             FRAMELIX_MODULE,
-            FileUtils::getUserdataFilepath("database.db", false)
+            FRAMELIX_DBDATA_FOLDER . "/sqlite_" . FRAMELIX_MODULE . ".db"
         );
     }
 }
