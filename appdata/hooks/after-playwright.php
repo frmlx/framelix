d7e539c35211419787afddaac0ad125acfb7ffa2
brainfoolong <brainfoolong@users.noreply.github.com>
2023-04-13 15:23:26 +0200

    added hook after playwright for more detailed messages

---
+++ appdata/hooks/after-playwright.php
@@ -0,0 +1,23 @@
+<?php
+
+$msg = 'Error';
+$color = '#FF2100';
+$msgFile = '/framelix/userdata/playwright/badge-message.txt';
+$colorFile = '/framelix/userdata/playwright/badge-color.txt';
+$coverage = "Failed";
+$resultsFile = "/framelix/userdata/playwright/results/results.json";
+if (file_exists($resultsFile)) {
+    $jsonData = json_decode(file_get_contents($resultsFile), true);
+    if (is_array($jsonData['errors'] ?? null) && !$jsonData['errors']) {
+        $tests = 0;
+        foreach ($jsonData['suites'] ?? [] as $suite) {
+            foreach ($suite['specs'] ?? [] as $spec) {
+                $tests += count($spec['tests']);
+            }
+        }
+        $msg = 'Passed ' . $tests . ' Tests';
+        $color = '#00FF59';
+    }
+}
+file_put_contents($msgFile, $msg);
+file_put_contents($colorFile, $color);
