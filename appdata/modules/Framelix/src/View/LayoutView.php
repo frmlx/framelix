ed2ddb75e7db363754ced38d1caade165d4bdb05
brainfoolong <brainfoolong@users.noreply.github.com>
2023-05-18 19:53:05 +0200

    updated browser requirements

--- appdata/modules/Framelix/src/View/LayoutView.php
+++ appdata/modules/Framelix/src/View/LayoutView.php
@@ -153,7 +153,6 @@ abstract class LayoutView extends View
         <head>
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <title><?= $this->getPageTitle(true) ?></title>
             <script>
               ;(function () {
