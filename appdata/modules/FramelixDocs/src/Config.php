5f929d5b56b1aee54551486efc5f89110dd99bf6
BrainFooLong <brainfoolong@users.noreply.github.com>
2024-04-09 15:49:33 +0200

    updates for new matchFilter in compiler

--- appdata/modules/FramelixDocs/src/Config.php
+++ appdata/modules/FramelixDocs/src/Config.php
@@ -2,8 +2,6 @@
 
 namespace Framelix\FramelixDocs;
 
-use Framelix\Framelix\View\Backend\View;
-
 use const FRAMELIX_MODULE;
 use const FRAMELIX_USERDATA_FOLDER;
 
@@ -16,15 +14,18 @@ class Config
 
     public static function onRegister(): void
     {
+        $matchFilterDefault = function (\Framelix\Framelix\View $view, string $position) {
+            return $view instanceof \Framelix\Framelix\View\Backend\View && $position === 'default';
+        };
         \Framelix\Framelix\Config::addSqliteConnection(
-          FRAMELIX_MODULE,
-          FRAMELIX_USERDATA_FOLDER . "/sqlite_" . FRAMELIX_MODULE . ".db"
+            FRAMELIX_MODULE,
+            FRAMELIX_USERDATA_FOLDER . "/sqlite_" . FRAMELIX_MODULE . ".db"
         );
         \Framelix\Framelix\Config::$languagesAvailable = ['en'];
         \Framelix\Framelix\Config::$backendLogoFilePath = __DIR__ . "/../public/images/logo.jpg";
 
         $bundle = \Framelix\Framelix\Config::createCompilerFileBundle("FramelixDocs", "scss", "docs");
-        $bundle->includeInBackendView = \Framelix\FramelixDocs\View\View::class;
+        $bundle->matchFilter = $matchFilterDefault;
         $bundle->addFile("scss/docs.scss");
 
         $bundle = \Framelix\Framelix\Config::createCompilerFileBundle("FramelixDocs", "scss", "landing");
@@ -32,17 +33,17 @@ class Config
         $bundle->addFile("../Framelix/scss/general/icon-font.scss");
 
         $bundle = \Framelix\Framelix\Config::createCompilerFileBundle("FramelixDocs", "scss", "vendor");
-        $bundle->includeInBackendView = \Framelix\FramelixDocs\View\View::class;
+        $bundle->matchFilter = $matchFilterDefault;
         $bundle->compile = false;
         $bundle->addFile("vendor/highlightjs/default.min.css");
         $bundle->addFile("vendor/highlightjs/atom-one-dark-reasonable.min.css");
 
         $bundle = \Framelix\Framelix\Config::createCompilerFileBundle("FramelixDocs", "js", "docs");
-        $bundle->includeInBackendView = \Framelix\FramelixDocs\View\View::class;
+        $bundle->matchFilter = $matchFilterDefault;
         $bundle->addFile("js/docs.js");
 
         $bundle = \Framelix\Framelix\Config::createCompilerFileBundle("FramelixDocs", "js", "vendor");
-        $bundle->includeInBackendView = \Framelix\FramelixDocs\View\View::class;
+        $bundle->matchFilter = $matchFilterDefault;
         $bundle->compile = false;
         $bundle->addFile("vendor/highlightjs/highlight.min.js");
         $bundle->addFile("vendor/highlightjs/highlightjs-line-numbers.min.js");
